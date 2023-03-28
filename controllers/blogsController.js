c9eab771becf9c07a6cb2ce6e3c0576aa8633ab7
johnmartinezc <john.martinez@codeimmersives.com>
2023-03-28 16:06:17 -0400

    working

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -1,6 +1,6 @@
 
 
-const Blog = require('../models/Blogs');
+const Blog = require('../model/Blogs');
 
 async function getAllBlogs(req, res) {
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
 
+const Blog = require('../model/Blogs');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res) {
+
+  //query blogs 
+  try {
+    const allBlogs = await Blog.find({});
+    res.json({blogs: allBlogs });
+  }catch(e){
+    console.log(e);
+  }
 });
+
 
 module.exports = router;
